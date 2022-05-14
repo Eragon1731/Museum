ad9012d607f9f1281b64daa297f23725c46ce7f0
Christy Ye <eragon1731@gmail.com>
2022-05-13 18:02:11 -0700

    updating camera controller

--- Assets/CameraButtonController.cs
+++ Assets/CameraButtonController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bda098f84954e7c89dd50e8f06e3fc73dfb0bf55d3479ef2e4cbaeeb64c3af69
-size 2381
+oid sha256:84f6acdf52108fc6f7b51c213d422367d174ca1f14f5c0395784b9ce9622bc3d
+size 3406
---
+++ Assets/_Scripts/Screenshot.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:adfc0c02be7ad4c914fff4bf1e5384dbb837cb1a71126006e10b8b5aca365c52
+size 422
