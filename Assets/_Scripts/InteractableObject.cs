0fc20febcc3f46add1245d49152291af92e757af
Christy Ye <eragon1731@gmail.com>
2022-05-14 13:54:15 -0700

    updating scene with new obj

--- Assets/_Scripts/InteractableObject.cs
+++ Assets/_Scripts/InteractableObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c06515d88e5adfa111be0c658fa4aaa85cbf2206a19959a39dd8793146d0d85
-size 2802
+oid sha256:c906210e60e3f48d596221426af09e886283731004b9e14cc7e6f18d65a268d4
+size 2782
