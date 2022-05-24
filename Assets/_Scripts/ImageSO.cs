6a65cb849bbfcf3b1e4751f4d1839c1c973b5098
Christy Ye <eragon1731@gmail.com>
2022-05-24 09:53:06 -0700

    adding image tracking

--- Assets/_Scripts/ImageSO.cs
+++ Assets/_Scripts/ImageSO.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cfc13e166d384eae920d42e4caa38b46e65720a910158fbf080002899b5084e
-size 440
+oid sha256:e7b0c1d180e207836cfd3192a8ce99fff81632c6ce8e941914f0435cd7f485b2
+size 516
--- Assets/_Scripts/TempManager.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3eaedda5690a6e681483cb80f15303542c2e346d034bc0b68bafda7361ef8b2b
-size 559
