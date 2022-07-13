686204e8b4e6e9628f3a75da36f386d6441ed7a3
Christy Ye <eragon1731@gmail.com>
2022-07-13 12:23:40 -0700

    removing counter

--- Assets/_Scripts/GardenChunk.cs
+++ Assets/_Scripts/GardenChunk.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae555f9e83fe6aa01bd8a135a985505a3f1cfd47ed5d96b5703acaf420ee1b01
-size 5628
+oid sha256:b7807f5933eb6e3b9a79b6a097cfe6760ba593c264c4ac799eca820412df2a84
+size 5630
