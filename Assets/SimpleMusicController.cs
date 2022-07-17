38644447223e319f258239fe23b6dce8ef8e8890
Christy Ye <eragon1731@gmail.com>
2022-07-17 11:16:49 -0700

    musicview placeholder

---
+++ Assets/SimpleMusicController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:82405a3f14a6fa492c56320309abb035873c778a51246b223846747c87cdb210
+size 504
--- Assets/_Scripts/AudioManager.cs
+++ Assets/_Scripts/AudioManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7050fa794711b27302f75c30925af178419d2a67fe9ec97ba93ce4c8ee3260a8
-size 544
+oid sha256:e397767c40641253c9c1b5aa1f1ffe857eea4b6c7a361c8eeb427cc83e43c32d
+size 619
